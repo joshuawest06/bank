4df0675f3e749a23f395aa61556f8c6aaf3ce44b
joshuawest06 <pprzwm40970@outlook.com>
2025-10-26 16:21:04 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const mockUSDT = await deploy("MockUSDT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const bank = await deploy("FixedTermBank", {
+    from: deployer,
+    args: [mockUSDT.address],
+    log: true,
+  });
+
+  console.log(`MockUSDT contract: ${mockUSDT.address}`);
+  console.log(`FixedTermBank contract: ${bank.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fixed_term_bank"; // id required to prevent reexecution
+func.tags = ["MockUSDT", "FixedTermBank"];
